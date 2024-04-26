54c894ff9964babab49fd75eae9e6b4577f238bd
diosfamiliafutbol56@gmail.com <diosfamiliafutbol56@gmail.com>
2024-04-26 17:30:54 -0500

    Hasta el momento funciona el actualizar

--- Desktop/NODE.JS-Miercoles-VIERNES/Proyecto-node.js/app/controller/controller.alumnos.js
+++ Desktop/NODE.JS-Miercoles-VIERNES/Proyecto-node.js/app/controller/controller.alumnos.js
@@ -1,19 +1,38 @@
 import { pool } from "../../config/db_mysql.js";
 
 export const crearalumno = async(req, res) =>{
-    try {
-        const resultado = await pool.query("select * from antiguedad");
-        console.log(resultado);
-        res.json(resultado);
-        
-    }catch (error) {
-        res.json({
-            "error":error,
-            "method":"post"
-        });
-    };  
+    
+        // const resultado = await pool.query("select * from antiguedad");
+        // console.log(resultado[0]);
+        // res.json(resultado[0]);
+        let info = req.body;
+        try {
+            let resultado = await pool.query(`
+            insert into propiedades (id_propiedad,
+                direccion,
+                tipo_propiedad)
+                values(${info.id_propiedad},'${info.direccion}','${info.tipo_propiedad}'
+                )
+            `);
 
-};
+            if(resultado[0].affectedRows > 0 ){
+                res.json({
+                    respuesta:"registro insertado"
+                })
+            }else{
+                res.json({
+                    "respuesta": "No inserto NADA"
+                })
+            }
+ 
+            
+        } catch (error) {
+            res.json({
+                "error":error,
+                "method":"post"
+            });  
+            };
+ };
 
 export const mostraralumno = async(req, res) =>{
 
@@ -32,19 +51,33 @@ export const mostraralumno = async(req, res) =>{
 };
 
 export const actualizaralumno = async(req, res) =>{
-
+    let info = req.body;
     try {
-        const resultado = await pool.query("select * from antiguedad");
-        console.log(resultado);
-        res.json(resultado);
+        let resultado = await pool.query(`
+        update propiedades
+        set
+        id_propiedad = ${info.id_propiedad},
+        direccion = '${info.direccion}',
+        tipo_propiedad = '${info.tipo_propiedad}'
+        where id_propiedad = ${info.id_propiedad}
+        `);
+
+        if (resultado[0].affectedRows > 0 ){
+            res.json({
+                respuesta:"registro modificado"
+            })
+        }else{
+            res.json({
+                "respuesta": "No modifico NADA"
+            });
+        };
         
-    }catch (error) {
+    } catch (error) {
         res.json({
             "error":error,
             "method":"put"
-        });
-    };  
-    
+        });  
+    };
 };
 
 export const eliminaralumno = async(req, res) =>{
